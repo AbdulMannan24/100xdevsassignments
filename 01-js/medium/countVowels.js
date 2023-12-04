3b8a5bfb83a80c321a66c1be286da11be2c456a0
Mohd Abdul Mannan <ma24042000@gmail.com>
2023-12-04 18:45:46 +0530

    assignment started

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,16 @@
 
 function countVowels(str) {
     // Your code here
+    let count = 0
+    let character;
+    for (let i = 0; i < str.length; i++) {
+        character = str[i];
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || 
+           character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+              count += 1;
+           }
+    }
+    return count;
 }
 
 module.exports = countVowels;
