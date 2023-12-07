617907a61c2e5ccee87867a63a1eca7d4666b443
Mohd Abdul Mannan <ma24042000@gmail.com>
2023-12-07 08:32:36 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,7 +7,18 @@
 function isAnagram(str1, str2) {
   if (str1.length != str2.length)
     return false;
-  
+  for (let i = 0; i < str1.length; i++) {
+    let flag = 0;
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i].toLowerCase() == str2[j].toLowerCase()) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0)
+      return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
